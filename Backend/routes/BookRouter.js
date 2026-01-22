901bb7c370d7cf562fc5769fae737489e614ab27
Kushagra Tiwari <kushagratiwari86@gmail.com>
2026-01-23 03:35:43 +0530

    Update delete created

--- Backend/routes/BookRouter.js
+++ Backend/routes/BookRouter.js
@@ -20,4 +20,31 @@ BookRouter.post('/add',async(req,res)=>{
     }
 })
 
+
+BookRouter.patch('/update',async(req,res)=>{
+    const {id}=req.params
+    const payload=req.body
+    try {
+        const updateBook=await BookModel.findByIdAndUpdate(id,payload,{new:true})
+        
+        res.status(200).json({message:`Book details of ${id} updated `,data:updateBook })
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+})
+
+
+BookRouter.delete('/update',async(req,res)=>{
+    const {id}=req.params
+    const payload=req.body
+    try {
+        const deleteBook=await BookModel.findByIdAndDelete(id)
+        
+        res.status(200).json({message:`Book details of ${id} deleted `,data:deleteBook })
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+})
+
+
 module.exports=BookRouter
